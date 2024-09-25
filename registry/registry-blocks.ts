014d1f0d58181f7b9ce83c737d6e4ed56feb62d4
developerwill <info@wilsongomes.me>
2024-09-25 11:11:31 +0000

    Add block login-page-01 to the registry

--- registry/registry-blocks.ts
+++ registry/registry-blocks.ts
@@ -1,3 +1,23 @@
 import { Registry } from '@/registry/schema';
 
-export const blocks: Registry = [];
+export const blocks: Registry = [
+  {
+    name: 'login-01',
+    type: 'registry:block',
+    dependencies: ['react-icons'],
+    registryDependencies: ['button', 'card', 'input', 'label'],
+    files: [
+      {
+        path: 'block/login-01/page.tsx',
+        target: 'app/login-01/page.tsx',
+        type: 'registry:page'
+      },
+      {
+        path: 'block/login-01/components/form.tsx',
+        type: 'registry:component'
+      }
+    ],
+    category: 'Authentication',
+    subcategory: 'Login'
+  }
+];
