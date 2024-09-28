ad784155090b87a9c5764167a786cbe6d7e86c60
developerwill <info@wilsongomes.me>
2024-09-28 17:41:24 +0000

    SEO update

---
+++ app/commonSEO.ts
@@ -0,0 +1,33 @@
+export const commonSEO = {
+  authors: [{ name: 'brezee/ui' }],
+  keywords: [
+    'breeze',
+    'ui',
+    'breezeui',
+    'breeze/ui',
+    'Next.js',
+    'React.js',
+    'NextJs',
+    'React',
+    'Tailwind CSS',
+    'Server Components',
+    'Radix UI'
+  ],
+  creator: 'developerwill'
+};
+
+export const commonOpenGraph = {
+  url: 'https://breeze-ui.wilsongomes.me',
+  siteName: 'breeze/ui',
+  locale: 'en_US',
+  images: [
+    {
+      url: 'https://breeze-ui.wilsongomes.me/og.jpg',
+      width: 1200,
+      height: 630,
+      alt: 'breeze/ui'
+    }
+  ],
+  type: 'website',
+  countryName: 'Portugal'
+};
