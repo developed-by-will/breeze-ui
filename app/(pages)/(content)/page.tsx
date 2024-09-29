0309398cf4edcf46f911e0813da40ec26839368b
developerwill <info@wilsongomes.me>
2024-09-29 00:04:23 +0000

    Remove unused file

--- app/(pages)/(content)/page.tsx
+++
@@ -1,9 +0,0 @@
-import Component from '../component/[slug]';
-
-type Props = {
-  params: { slug: string };
-};
-
-export default async function Index({ params }: Readonly<Props>) {
-  return <Component slug={params.slug} />;
-}
