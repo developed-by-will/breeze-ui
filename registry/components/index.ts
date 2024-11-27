dd496aeaf4ec23851275ddb9bbf855f555a7bfc0
developerwill <info@wilsongomes.me>
2024-11-27 10:09:02 -0100

    Refactoring

---
+++ registry/components/index.ts
@@ -0,0 +1,15 @@
+import * as Login01 from './block/login-01/components/config';
+import * as Breadcrumbs from './ui/breadcrumbs/config';
+import * as Code from './ui/code/config';
+import * as Steps from './ui/steps/config';
+import * as SyntaxHighlighter from './ui/syntaxHighlighter/config';
+import * as Toast from './ui/toast/config';
+
+export const componentsConfig = [
+  Breadcrumbs.config,
+  Code.config,
+  SyntaxHighlighter.config,
+  Steps.config,
+  Toast.config
+];
+export const blocksConfig = [Login01.config];
