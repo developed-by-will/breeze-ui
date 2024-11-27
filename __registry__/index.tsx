b1894ed2f50bb4f32f84b97d1cdba5d2dda6def0
developerwill <info@wilsongomes.me>
2024-11-27 12:53:38 -0100

    Ran prettier

--- __registry__/index.tsx
+++ __registry__/index.tsx
@@ -96,17 +96,6 @@ export const Index: Record<string, unknown> = {
       category: 'undefined',
       subcategory: 'undefined',
       chunks: []
-    },
-    'use-toast': {
-      name: 'use-toast',
-      type: 'registry:hook',
-      registryDependencies: undefined,
-      files: ['registry/components/ui/toast/hooks/use-toast.ts'],
-      component: React.lazy(() => import('@/registry/components/ui/toast/hooks/use-toast.ts')),
-      source: '',
-      category: 'undefined',
-      subcategory: 'undefined',
-      chunks: []
     }
   }
 };
